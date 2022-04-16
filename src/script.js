e8efe47525cca2fcb7f9d0b61fb5d16c63784fa4
alexamklein <99451706+alexamklein@users.noreply.github.com>
2022-04-15 23:16:07 -0700

    Changed time display from military to AM/PM and change time display to time in searched city instead of local time

--- src/script.js
+++ src/script.js
@@ -1,30 +1,46 @@
-function formatDate(date) {
+function formatCityTime(date) {
   let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
-  let currentDay = days[date.getDay()];
-  let currentHour = date.getHours();
-  if (currentHour < 10) {
-    currentHour = `0${currentHour}`;
+  let cityDay = days[date.getDay()];
+  let hours = [
+    12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10,
+    11,
+  ];
+  let cityHour = hours[date.getHours()];
+  let cityMinute = date.getMinutes();
+  if (cityMinute < 10) {
+    cityMinute = `0${cityMinute}`;
   }
-  let currentMinute = date.getMinutes();
-  if (currentMinute < 10) {
-    currentMinute = `0${currentMinute}`;
+  let militaryHour = date.getHours();
+  if (militaryHour < 12) {
+    amPm = "AM";
+  } else {
+    amPm = "PM";
   }
-  return `${currentDay}, ${currentHour}:${currentMinute}`;
+  return `${cityDay}, ${cityHour}:${cityMinute} ${amPm}`;
+}
+
+function getCityTime(response) {
+  let localTime = new Date(response.data.dt * 1000);
+  let utcOffset = localTime.getTimezoneOffset() * 60000;
+  let utcTime = response.data.dt * 1000 + utcOffset;
+  let cityTimeStamp = utcTime + response.data.timezone * 1000;
+  let cityTime = new Date(cityTimeStamp);
+  document.querySelector("#day-time").innerHTML = formatCityTime(cityTime);
 }
 
 function displayWeatherConditions(response) {
   document.querySelector("#city").innerHTML = response.data.name;
-  document.querySelector("#temperature").innerHTML = Math.round(
-    response.data.main.temp
-  );
   document.querySelector("#weather-description").innerHTML =
     response.data.weather[0].main;
-  document.querySelector("#feels-like").innerHTML = Math.round(
-    response.data.main.feels_like
-  );
   document.querySelector("#humidity").innerHTML = response.data.main.humidity;
   document.querySelector("#wind").innerHTML = Math.round(
     response.data.wind.speed
+  );
+  document.querySelector("#temperature").innerHTML = Math.round(
+    response.data.main.temp
+  );
+  document.querySelector("#feels-like").innerHTML = Math.round(
+    response.data.main.feels_like
   );
 }
 
@@ -33,6 +49,7 @@ function displayOnLoad(city) {
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiKey = "6f7fc1e8921ca5e8743c4596d4b381f9";
   let apiUrl = `${apiEndpoint}?q=${city}&appid=${apiKey}&units=${units}`;
+  axios.get(`${apiUrl}`).then(getCityTime);
   axios.get(`${apiUrl}`).then(displayWeatherConditions);
 }
 
@@ -49,15 +66,14 @@ function searchCurrentLocation(position) {
   let apiEndpoint = "https://api.openweathermap.org/data/2.5/weather";
   let apiKey = "6f7fc1e8921ca5e8743c4596d4b381f9";
   let apiUrl = `${apiEndpoint}?lat=${latitude}&lon=${longitude}&appid=${apiKey}&units=${units}`;
+  axios.get(`${apiUrl}`).then(getCityTime);
   axios.get(`${apiUrl}`).then(displayWeatherConditions);
 }
+
 function getCurrentLocation(event) {
   event.preventDefault();
   navigator.geolocation.getCurrentPosition(searchCurrentLocation);
 }
-
-let currentTime = new Date();
-document.querySelector("#local-time").innerHTML = formatDate(currentTime);
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchCity);
